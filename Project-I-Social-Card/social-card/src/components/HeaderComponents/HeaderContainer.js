1b1ac76dbfb793aedddec4cf252aacfca863435d
conconrob <connortprobertson@gmail.com>
2019-02-11 13:56:47 +0000

    1st draft of header component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail.js';
+import HeaderTitle from './HeaderTitle.js';
+import HeaderContent from './HeaderContent.js';
+export default function HeaderContainer() {
+  return (
+    <div className="header-container">
+      <ImageThumbnail />
+      <div>
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+      
+    </div>
+  );
+}
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+export default function HeaderTitle() {
+    return (
+        <div className='header-title-container' >
+            <h2>Lambda School</h2>
+            <p className="header-author">@LambdaSchool</p>
+            <p className="header-date">26 Jan</p>
+        </div >
+    );
+}
